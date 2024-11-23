38ff0494b57dfdc73c1160672cf188a69d7221b2
Chaitanya <chaitanyapothala63@gmail.com>
2024-11-23 12:21:23 +0530

    Updated Home page with dashboard link

--- app/page.js
+++ app/page.js
@@ -1,11 +1,16 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h2>Welcome to My New Project</h2>
-      <Button>Subscribe</Button>
+    <div className="text-center p-4">
+      <h2 className="text-xl font-bold mb-4">Welcome to My New Project</h2>
+      
+      {/* Link to Dashboard */}
+      <Link href="/dashboard">
+        <Button>Go to Dashboard</Button>
+      </Link>
     </div>
   );
 }
